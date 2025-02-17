d685ad76f97b376597c267032462f99fae97e98f
Nyantai12 <112607105+Nyantai12@users.noreply.github.com>
2025-02-17 17:02:58 +0800

    expo router done

---
+++ exporouter/exporouter/app/(tabs)/about.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link, router } from "expo-router";
+import { View, Text, Pressable } from "react-native";
+
+const About = () => {
+ return (
+   <View>
+     <Text>About</Text>  
+   </View>);};
+
+export default About;
---
+++ exporouter/exporouter/app/aboutus.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link, router } from "expo-router";
+import { View, Text, Pressable } from "react-native";
+
+const Aboutus = () => {
+ return (
+   <View>
+     <Text>Aboutus</Text>  
+   </View>);};
+
+export default Aboutus;
